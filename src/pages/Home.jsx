2f97e815e9c127e54fa14a5ed30d2d7512725648
smvastrorajaram-hub <smvastrorajaram@gmail.com>
2026-08-07 09:29:07 +0530

    Update Home with Search Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import SearchSection from "../components/SearchSection";
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <SearchSection />
     </>
   );
 }
